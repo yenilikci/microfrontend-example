78fa256c9dbb87e3e91253ea1aaad372379203eb
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-08-15 23:04:45 +0300

    building a microfrontend

---
+++ barchart/src/index.js
@@ -0,0 +1,26 @@
+import * as echarts from "echarts";
+
+// Initialize the echarts instance based on the prepared dom
+var myChart = echarts.init(document.getElementById('barchart'));
+
+// Display the chart using the configuration items and data just specified.
+myChart.setOption({
+    title: {
+        text: 'ECharts Getting Started Example'
+    },
+    tooltip: {},
+    legend: {
+        data: ['sales']
+    },
+    xAxis: {
+        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+    },
+    yAxis: {},
+    series: [
+        {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }
+    ]
+});
